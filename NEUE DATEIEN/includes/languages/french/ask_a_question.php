a61b48b7c988f5324f43622b445e0c936fdd6f36
Jörg Schönberger <office@webchills.at>
2022-12-07 19:48:02 +0100

    ask_a_question ergänzt

--- NEUE DATEIEN/includes/languages/french/ask_a_question.php
+++ NEUE DATEIEN/includes/languages/french/ask_a_question.php
@@ -3,26 +3,25 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
- * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * translation 2021 by klartexter
- * @version $Id: ask_a_question.php 2021-12-27 21:02:53Z webchills $
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0 
+ * @version $Id: ask_a_question.php 2022-12-07 19:02:53Z webchills $
  */
-define('NAVBAR_TITLE', 'Frage zum Artikel stellen');
+define('NAVBAR_TITLE', 'Poser une question sur le produit');
 
-define('HEADING_TITLE', 'Frage stellen zu ');
-define('FORM_TITLE', 'Was ist Ihre Frage?');
+define('HEADING_TITLE', 'Poser une question sur ');
+define('FORM_TITLE', 'Quelle est votre question ?');
 
-define('TEXT_SUCCESS', 'Ihre Nachricht wurde erfolgreich versandt.');
-define('EMAIL_SUBJECT', 'Artikelanfrage an ' . STORE_NAME);
+define('TEXT_SUCCESS', 'Votre message a été envoyé avec succès.');
+define('EMAIL_SUBJECT', 'Question sur un produit à ' . STORE_NAME);
 
-define('ENTRY_NAME', 'Ihr Name:');
-define('ENTRY_EMAIL', 'Email Adresse:');
-define('ENTRY_TELEPHONE', 'Telefonnummer:');
-define('ENTRY_ENQUIRY', 'Nachricht:');
-define('TEXT_PRODUCT_NAME', 'Artikelname: ');
+define('ENTRY_NAME', 'Votre nom:');
+define('ENTRY_EMAIL', 'Votre e-mail:');
+define('ENTRY_TELEPHONE', 'Votre numéro de téléphone:');
+define('ENTRY_ENQUIRY', 'Votre message:');
+define('TEXT_PRODUCT_NAME', 'Nom du produit: ');
 
-define('SEND_TO_TEXT','Email senden an:');
-define('ENTRY_EMAIL_NAME_CHECK_ERROR','Sorry, ist Ihr Name korrekt? Unser System benötigt mindestens ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' Zeichen. Bitte erneut eingeben.');
-define('ENTRY_EMAIL_CONTENT_CHECK_ERROR','Haben Sie Ihre Nachricht vergessen? Bitte ins Textfeld unten eingeben.');
+define('SEND_TO_TEXT','Envoyer un e-mail à:');
+define('ENTRY_EMAIL_NAME_CHECK_ERROR','Désolé, votre nom est-il correct ? Notre système a besoin d\'au moins des ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' caractères. Veuillez saisir à nouveau.');
+define('ENTRY_EMAIL_CONTENT_CHECK_ERROR','Vous avez oublié votre message ? Veuillez le saisir dans le champ de texte ci-dessous.');
 
-define('NOT_LOGGED_IN_TEXT', 'Nicht eingeloggt');
+define('NOT_LOGGED_IN_TEXT', 'pas connecté');
